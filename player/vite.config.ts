5578b40709510ca0acc951430d8ee774c570d16d
Frosetrain <80436189+frosetrain@users.noreply.github.com>
2025-01-25 08:33:29 +0800

    Add HTTPS for localhost

--- player/vite.config.ts
+++ player/vite.config.ts
@@ -1,6 +1,8 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import tailwindcss from "tailwindcss";
+import fs from "fs";
 
 export default defineConfig({
     css: {
@@ -8,5 +10,12 @@ export default defineConfig({
             plugins: [tailwindcss()],
         },
     },
-    plugins: [sveltekit()],
+    plugins: [sveltekit(), basicSsl()],
+    server: {
+        https: {
+            key: fs.readFileSync(`${__dirname}/cert/key.pem`),
+            cert: fs.readFileSync(`${__dirname}/cert/cert.pem`),
+        },
+        proxy: {},
+    },
 });
